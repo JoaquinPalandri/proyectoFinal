2b3dfc34060892d52a0a384b79911e5ecaca7d64
JoaquinPalandri <joaquin.palandri@uner.edu.ar>
2024-05-20 19:34:06 -0300

    actualizacion

--- backend_MQTT.js
+++ backend_MQTT.js
@@ -1,6 +1,5 @@
 const mqtt = require("mqtt");
-const mysql= require("mysql");
-
+const mysql = require("mysql");
 
 const mqttBroker = 'wss://mqtt.eclipseprojects.io/mqtt'; // Cambiar al URI de tu broker MQTT
 const client = mqtt.connect(mqttBroker, {
@@ -12,6 +11,7 @@ client.on('connect', function () {
     // Suscribirse al tema para recibir mensajes
     client.subscribe('BetweenRivers/EquiposDuchaCama');
 });
+
 client.on('message', function (topic, message) {
     // Mensaje recibido del broker MQTT
     console.log('Mensaje recibido en el tema', topic, ':', message.toString());
@@ -34,33 +34,35 @@ client.on('message', function (topic, message) {
         const ciclos = partes[9];
         const temperatura = partes[10];
 
-        // Aquí puedes agregar la lógica para manejar los datos extraídos
-        console.log('Código de equipo:', codEquipo);
-        console.log('ID:', id);
-        console.log('Fecha:', fecha);
-        console.log('Retrolavado:', retrolavado);
-        console.log('Conexión:', conexionMensaje);  // Usar el nombre diferente
-        console.log('Tiempo de uso:', tiempoUso);
-        console.log('Tiempo de aspirado:', tiempoAsp);
-        console.log('Tiempo de baño:', tiempoBaño);
-        console.log('Ciclos:', ciclos);
-        console.log('Temperatura:', temperatura);
-        
+        // Crear conexión a la base de datos MySQL
         const conexionDB = mysql.createConnection({
             host: "boczfz9qtkrjwks7pcyg-mysql.services.clever-cloud.com",
             database: "boczfz9qtkrjwks7pcyg",
             user: "uyvaseavx4jwx0ly",
             password: "1UbO1UYZfUkPx29ijloL",
         });
-        
-        conexionDB.connect(function(err) {
+
+        // Insertar datos en la base de datos
+        const query = `INSERT INTO Equipo333 (id, retrolavado, conexion, tiempoUso, tiempoAsp, tiempoBaño, ciclos, temperatura) VALUES ('${id}', '${retrolavado}', '${conexionMensaje}', '${tiempoUso}', '${tiempoAsp}', '${tiempoBaño}', '${ciclos}', '${temperatura}')`;
+
+        conexionDB.connect(function (err) {
             if (err) {
                 throw err;
             } else {
-                console.log("Conexión a la base de datos exitosaa");
+                // Ejecutar la consulta
+                conexionDB.query(query, function (error, results, fields) {
+                    if (error) {
+                        console.log(error);
+                        throw error;
+                    } else {
+                        console.log("Datos insertados correctamente");
+                    }
+                    // Cerrar la conexión después de la consulta
+                    conexionDB.end();
+                });
             }
         });
-        
+
     } else {
         console.error('El mensaje no tiene la estructura esperada');
     }
